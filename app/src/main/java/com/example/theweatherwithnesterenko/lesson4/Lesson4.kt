39a9d79f54821055069a85bef1e72562640b0c45
ThePhyrus <romaisgood@yandex.ru>
2022-04-16 16:15:36 +0300

    Урок 4. познакомился с функцией высшего порядка.

--- app/src/main/java/com/example/theweatherwithnesterenko/lesson4/Lesson4.kt
+++ app/src/main/java/com/example/theweatherwithnesterenko/lesson4/Lesson4.kt
@@ -10,7 +10,7 @@ class Lesson4 {
         lesson3.usual1("что-то произошло 1 способ")
     }
 
-    var f = fun(string: String){}
+    var f = fun(string: String) {}
     fun some2() { // 2 способ
         f("что-то произошло 2 способ")
     }
@@ -18,20 +18,48 @@ class Lesson4 {
 
     lateinit var speakable: Speakable
 
-    fun some3(){  // 3.1 способ
+    fun some3() {  // 3.1 способ
         speakable.f("что-то произошло 3.1 способ", 1)
     }
 
-    fun some4(){  // 3.2 способ
+    fun some4() {  // 3.2 способ
         speakable.f("что-то произошло 3.2 способ", 1)
     }
 
 
-    fun some5(){  // 4.1 способ
+    fun some5() {  // 4.1 способ
         speakable.f("что-то произошло 4.1 способ", 1)
     }
-    fun some6(_speakable: Speakable){  // 4.2 способ
+
+    fun some6(_speakable: Speakable) {  // 4.2 способ
         _speakable.f("что-то произошло 4.2 способ", 1)
+    }
+
+
+    val _f = fun(float: Float): Unit {
+
+    }
+
+    val __f = fun(double: Double): Short {
+        return 1
+    }
+
+    val _c = fun(char: Char): Boolean {
+        return true
+    }
+
+    fun funHigh(
+        _f: (float: Float) -> Unit,
+        __f: (double: Double) -> Short,
+        _c: (char: Char) -> Boolean
+    ) {
+        _f(1f)
+        val short = __f(1.0)
+        val boolean = _c('@')
+    }
+
+    fun main(){
+        funHigh(_f, __f, _c)
     }
 
 }
