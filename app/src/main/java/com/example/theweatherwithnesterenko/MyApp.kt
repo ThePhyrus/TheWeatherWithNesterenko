7d9e8e74e58b4c2c996c28c6ca2b85c45fc5cec5
ThePhyrus <romaisgood@yandex.ru>
2022-05-11 21:28:47 +0300

    Урок 9. Коммит перед факультативом (создание собственного контент-провайдера).

--- app/src/main/java/com/example/theweatherwithnesterenko/MyApp.kt
+++ app/src/main/java/com/example/theweatherwithnesterenko/MyApp.kt
@@ -8,35 +8,36 @@ import com.example.theweatherwithnesterenko.domain.room.HistoryDao
 import com.example.theweatherwithnesterenko.domain.room.MyDB
 import java.lang.IllegalStateException
 
-class MyApp:Application() {
+class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         appContext = this
     }
-    companion object{
+
+    companion object {
         private var db: MyDB? = null
         private var appContext: MyApp? = null
-        fun getHistoryDao(): HistoryDao{
-            if (db == null){
-                if (appContext != null){
+        fun getHistoryDao(): HistoryDao {
+            if (db == null) {
+                if (appContext != null) {
 //                    Thread{ //todo try 2 variant
-                        db = Room.databaseBuilder(appContext!!,MyDB::class.java,"test")
-                            .allowMainThreadQueries() // todo HW FOR CHEATERS ONLY
-                            .addMigrations(migration_1_2)
-                            .build()
+                    db = Room.databaseBuilder(appContext!!, MyDB::class.java, "test")
+                        .allowMainThreadQueries() // todo HW FOR CHEATERS ONLY
+                        .addMigrations(migration_1_2)
+                        .build()
 //                    }.start() //todo try 2 variant
-                }else {
+                } else {
                     throw IllegalStateException("что-то пошло не так и у нас пустой appContext")
                 }
             }
             return db!!.historyDao()
         }
-        private val migration_1_2: Migration = object : Migration(1,2){
+
+        private val migration_1_2: Migration = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE history_table ADD column condition TEXT NOT NULL DEFAULT ''")
             }
-
         }
     }
 }
