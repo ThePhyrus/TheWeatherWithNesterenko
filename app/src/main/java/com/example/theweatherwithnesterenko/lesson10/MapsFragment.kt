c5c86209a9bc71a5f120db5430f6aa97c40b76a9
ThePhyrus <romaisgood@yandex.ru>
2022-05-13 16:54:36 +0300

    Урок 10. Настроили интерфейс карт.

--- app/src/main/java/com/example/theweatherwithnesterenko/lesson10/MapsFragment.kt
+++ app/src/main/java/com/example/theweatherwithnesterenko/lesson10/MapsFragment.kt
@@ -1,26 +1,32 @@
 package com.example.theweatherwithnesterenko.lesson10
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.theweatherwithnesterenko.R
 import com.example.theweatherwithnesterenko.databinding.FragmentMapsMainBinding
-import com.example.theweatherwithnesterenko.repository.City
-import com.example.theweatherwithnesterenko.repository.Weather
+import com.example.theweatherwithnesterenko.utils.REQUEST_CODE_FOR_PERMISSION_TO_ACCESS_FINE_LOCATION
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 class MapsFragment : Fragment() {
     private var _binding: FragmentMapsMainBinding? = null
     private val binding: FragmentMapsMainBinding get() = _binding!!
+    private lateinit var map: GoogleMap
+    private val markers: ArrayList<Marker> = arrayListOf()
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -32,12 +38,59 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+
+        map = googleMap
         val saintPetersburg = LatLng(59.939099, 30.315877)
-        googleMap.addMarker(
+        map.addMarker(
             MarkerOptions().position(saintPetersburg).title("Marker in Saint-Petersburg")
         )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(saintPetersburg))
+        map.moveCamera(CameraUpdateFactory.newLatLng(saintPetersburg))
+        map.setOnMapLongClickListener {
+            addMarkerToArray(it)
+            drawLine()
+        }
+        map.uiSettings.isZoomControlsEnabled = true // появятся "+" и "-" для ZOOM
+        map.uiSettings.isMyLocationButtonEnabled = true
+        map.isMyLocationEnabled = true // todo add permission check
+
     }
+
+/*
+    private fun checkPermission() { //todo play with debugger to understand order of execution
+        //а есть ли разрешение? Проверим
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            map.uiSettings.isMyLocationButtonEnabled = true
+            map.isMyLocationEnabled = true
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            explain()
+        } else {
+            mRequestPermission()
+        }
+    }
+
+    private fun explain() {//FIXME
+        AlertDialog.Builder(requireContext())
+            .setTitle(resources.getString(R.string.dialog_address_title))
+            .setMessage(resources.getString(R.string.dialog_rationale_message))
+            .setPositiveButton(resources.getString(R.string.dialog_rationale_give_access)) { _, _ ->
+                mRequestPermission()
+            }
+            .setNegativeButton(R.string.close_access) { dialog, _ -> dialog.dismiss() }
+            .create()
+            .show()
+    }
+
+    private fun mRequestPermission() {
+        requestPermissions(
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_CODE_FOR_PERMISSION_TO_ACCESS_FINE_LOCATION
+        )
+    }
+*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,6 +104,37 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    private fun addMarkerToArray(location: LatLng) {
+        val marker = setMarker(location, markers.size.toString(), R.drawable.ic_map_pin)
+        markers.add(marker)
+    }
+
+    private fun drawLine(){
+        var previousMarker: Marker? = null
+        markers.forEach { currentPosition->
+            previousMarker?.let{ previousPosition->
+                map.addPolyline(
+                    PolylineOptions().add(previousPosition.position,currentPosition.position)
+                        .color(Color.RED)
+                        .width(5f))
+            }
+            previousMarker = currentPosition
+        }
+    }
+
+    private fun setMarker(
+        location: LatLng,
+        searchText: String,
+        resourceId: Int
+    ): Marker {
+        return map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(searchText)
+                .icon(BitmapDescriptorFactory.fromResource(resourceId))
+        )!!
     }
 
     override fun onDestroy() {
